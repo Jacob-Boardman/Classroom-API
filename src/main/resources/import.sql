8e017607aaa2b227a7d62935bbd59db21ce92a3d
Your Name <jacob.baordman.jb@gmail.com>
2019-01-18 12:17:52 +0000

    changed the sql file and file locations

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO Classroom (id, trainer, trainees) VALUES (1, 'John Gordan');
+INSERT INTO Classroom (id, trainer, trainees) VALUES (2, 'Matt Hunt');
+INSERT INTO Trainee (id, name) VALUES (1, 'Joe Bloggs');
+INSERT INTO Trainee (id, name) VALUES (2, 'Jane Bloggs');
+INSERT INTO Trainee (id, name) VALUES (3, 'David Bloggs');
+INSERT INTO Trainee (id, name) VALUES (4, 'Ceri Bloggs');
--- src/test/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Account (id, firstName, lastName, accountNumber) VALUES (1, 'John', 'Doe', '1234');
-INSERT INTO Account (id, firstName, lastName, accountNumber) VALUES (2, 'Jane', 'Doe', '1235');
-INSERT INTO Account (id, firstName, lastName, accountNumber) VALUES (3, 'Jim', 'Taylor', '1236');
