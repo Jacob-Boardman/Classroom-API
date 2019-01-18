e8093a5f7d4ae724486ef70e2e3bbe07feb4775f
Your Name <jacob.baordman.jb@gmail.com>
2019-01-18 11:02:56 +0000

    Added methods to service

--- src/main/java/com/qa/service/ClassroomService.java
+++ src/main/java/com/qa/service/ClassroomService.java
@@ -1,5 +1,13 @@
 package com.qa.service;
 
 public interface ClassroomService {
+	
+	String createClassroom(String classroom);
+	
+	String getAllClassrooms();
+	
+	String updateClassroom(Long id, String classroom);
+	
+	String deleteClassroom(Long id);
 
 }
--- src/main/java/com/qa/service/TraineeService.java
+++ src/main/java/com/qa/service/TraineeService.java
@@ -1,5 +1,13 @@
 package com.qa.service;
 
 public interface TraineeService {
+	
+	String createTrainee(String trainee);
+	
+	String getTrainee();
+	
+	String updateTrainee(Long id, String trainee);
+	
+	String deleteTrainee(Long id);
 
 }
