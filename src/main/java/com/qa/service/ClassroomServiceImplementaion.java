4105c483c6c88dbded60d373cc212c3cf4d0d056
Your Name <jacob.baordman.jb@gmail.com>
2019-01-18 11:41:15 +0000

    made basic Service

--- src/main/java/com/qa/service/ClassroomServiceImplementaion.java
+++ src/main/java/com/qa/service/ClassroomServiceImplementaion.java
@@ -1,5 +1,33 @@
 package com.qa.service;
 
-public class ClassroomServiceImplementaion {
+import javax.inject.Inject;
+
+import com.qa.persistence.repository.ClassroomRepository;
+
+
+public class ClassroomServiceImplementaion implements ClassroomService {
+	
+	@Inject
+	private ClassroomRepository repo;
+	
+	public String createClassroom(String classroom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAllClassrooms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateClassroom(Long id, String classroom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String deleteClassroom(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/main/java/com/qa/service/TraineeServiceImplementation.java
+++ src/main/java/com/qa/service/TraineeServiceImplementation.java
@@ -1,5 +1,33 @@
 package com.qa.service;
 
-public class TraineeServiceImplementation {
+import javax.inject.Inject;
+
+import com.qa.persistence.repository.TraineeRepository;
+
+public class TraineeServiceImplementation implements TraineeService {
+	
+	@Inject
+	private TraineeRepository repo;
+
+
+	public String createTrainee(String trainee) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getTrainee() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateTrainee(Long id, String trainee) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String deleteTrainee(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
